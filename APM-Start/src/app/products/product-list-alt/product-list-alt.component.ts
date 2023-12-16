a034b16c46521abab618b40c62276504b56eb070
AP\amitkumar.siddhapura <amitkumar.siddhapura@wolterskluwer.com>
2023-12-16 17:49:51 +0530

    Declarative approach

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { EMPTY, Subscription, catchError } from 'rxjs';
 
-import { Product } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -14,20 +13,21 @@ export class ProductListAltComponent implements OnInit, OnDestroy {
   errorMessage = '';
   selectedProductId = 0;
 
-  products: Product[] = [];
-  sub!: Subscription;
-
+  products$ = this.productService.products$
+  .pipe(
+    catchError(err => {
+      this.errorMessage= err;
+      return EMPTY;
+    })
+  );
+  
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.sub = this.productService.getProducts().subscribe({
-      next: products => this.products = products,
-      error: err => this.errorMessage = err
-    });
   }
 
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
+    
   }
 
   onSelected(productId: number): void {
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,35 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { EMPTY, Subscription, catchError } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 
-import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnDestroy {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products: Product[] = [];
-  sub!: Subscription;
+  products$ = this.productService.products$.pipe(
+    catchError(err => {
+      this.errorMessage= err;
+      return EMPTY;
+    })
+  );
 
   constructor(private productService: ProductService) { }
 
-  ngOnInit(): void {
-    this.sub = this.productService.getProducts()
-      .subscribe({
-        next: products => this.products = products,
-        error: err => this.errorMessage = err
-      });
-  }
-
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
   }
 
   onAdd(): void {
